3a7a2a1154e7cce298d42413618e277c8dca9e2e
unknown <agustinurteaga@gmail.com>
2022-01-24 22:41:23 -0300

    Tp 2 V1

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import '../components/NavBar.css'
+
+const NavBar = () => {
+return (
+    <nav className='Navigation'>
+        <ul>
+            <li>Listado </li>
+            <li>Listado 2</li>
+            <li>Listado 3 </li>
+            <li>Listado 4 </li>
+        </ul>
+    </nav>
+)
+}
+export default NavBar;
